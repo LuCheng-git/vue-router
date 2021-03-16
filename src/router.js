fb7b176b5cfad5e9e3141deb557d667ffc70e28b
LuCheng-git <1456096100@qq.com>
2021-03-16 23:24:13 +0800

    create  404 Not Found

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 
 import Home from './views/Home.vue'
 
+import store from './store'
+
 Vue.use(Router)
 
 export default new Router({
@@ -27,7 +29,23 @@ export default new Router({
             props: true,
             component: () => import(/*webpackChunkName: 'ExperienceDetails' */ './views/ExperienceDetails.vue')
           }
-        ]
+        ],
+        beforeEnter:(to, from, next) => {
+          const exists = store.destinations.find(
+            destination => destination.slug === to.params.slug
+          ) 
+          if(exists) {
+            next()
+          }else {
+            next({name:"notFound"})
+          }
+        },
       },
+      {
+        path: "/404",
+        alias: '*',
+        name: "notFound",
+        component: () => import(/*webpackChunkName: 'NotFound'*/ './views/NotFound.vue')
+      }
     ]
 })
